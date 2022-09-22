38561059d638cbaf3969ad490001343876ebc0de
adibradu <adrian.bradu@gmail.com>
2022-09-22 11:04:34 +0300

    IMNOT web app v2.0 first deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.use(cors());
+
+app.listen(process.env.PORT, () => console.log("IMNOT server running..."));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import './App.scss';
+import SectionLandingScreen from './components/Organism/SectionLandingScreen/SectionLandingScreen.component';
+import SectionProjects from './components/Organism/SectionProjects/SectionProjects.component';
+import SectionGroupEGP from './components/Organism/SectionGroupEGP/SectionGroupEGP.component.jsx';
+import Footer from './components/Organism/Footer/Footer.componenet.jsx';
+import { Helmet } from 'react-helmet';
+import { AppContext } from './contexts/AppContext';
+import { documentHeight, wathcResize } from './utils.js';
 
 function App() {
+  const { themeColorObj } = useContext(AppContext);
+  const themeColorDark = themeColorObj[0];
+
+  documentHeight();
+  wathcResize();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={themeColorDark ? 'dark' : 'light'}>
+      <Helmet>
+        <title>IMNOT</title>
+        <meta
+          name="description"
+          content="IMNOT designs user interfaces and builds web applications, with passion, simplicity and ambition."
+        />
+      </Helmet>
+      <SectionLandingScreen />
+      <SectionProjects />
+      <SectionGroupEGP />
+      <Footer />
     </div>
   );
 }
